0cbe2096cff55de0f03fea139b0789ed1bbd9f55
Jakub Hlavačka <jakubhlavacka123@gmail.com>
2023-02-22 19:52:51 +0100

    work wit hash_function and experiment_name from request body

--- blueprints/files_producer_routes.py
+++ blueprints/files_producer_routes.py
@@ -8,11 +8,24 @@ from hashlib import sha256
 from producers.files_producer import set_up_producer, delivery_report
 from serialization_classes.file_data import FileData
 
+
+def handle_not_valid_request(res, message):
+    res = jsonify({ 'message': message })
+    res.status_code = 400
+    return res
+
+
 time.sleep(30)
 
 MIN_SIZE = 64
 AVG_SIZE = 256
 MAX_SIZE = 1024
+
+HASH_FUNCTIONS = {
+    'sha256': sha256,
+    'md5': None,
+    'sha1': None
+}
 
 producer = set_up_producer()
 
@@ -20,21 +33,19 @@ files_producer_routes = Blueprint('files_producer_routes', __name__)
 
 @files_producer_routes.route('/upload', methods=['POST'])
 def upload_file():
-    # handling file source: https://roytuts.com/python-flask-rest-api-file-upload/
+    if 'experiment_name' not in request.form:
+        return handle_not_valid_request(res, 'Provide any experiment name in experiment_name field.')
+
+    if 'hash_function' not in request.form or request.form.get('hash_function') not in HASH_FUNCTIONS:
+        return handle_not_valid_request(res, 'Provide valid hash function (sha256, md5 or sha1) in hash_function field')
+
+    # handling file, source: https://roytuts.com/python-flask-rest-api-file-upload/
     if 'file' not in request.files:
-        res = jsonify({
-            'message': 'There is no file in part of request.'
-        })
-        res.status_code = 400
-        return res
+        return handle_not_valid_request(res, 'There is no file in part of request.')
 
     uploaded_file = request.files['file']
     if uploaded_file.filename == '':
-        res = jsonify({
-            'message': 'No file was uploaded.'
-        })
-        res.status_code = 400
-        return res
+        return handle_not_valid_request('No file was uploaded.')
 
     try:
         min_size = int(request.form.get('min_size')) if 'min_size' in request.form \
@@ -43,6 +54,8 @@ def upload_file():
             and int(request.form.get('avg_size')) and int(request.form.get('avg_size')) > AVG_SIZE else AVG_SIZE
         max_size = int(request.form.get('max_size')) if 'max_size' in request.form \
             and int(request.form.get('max_size')) and int(request.form.get('max_size')) > MAX_SIZE else MAX_SIZE
+        
+        hash_function = HASH_FUNCTIONS[request.form.get('hash_function')]
 
         file_content_bytes = uploaded_file.read()
 
@@ -50,8 +63,8 @@ def upload_file():
         
         # perform chunking and time it takes
         start = time.perf_counter_ns()
-        chunks = list(fastcdc(file_content_bytes, min_size=min_size, 
-            avg_size=avg_size, max_size=max_size, fat=True, hf=sha256))
+        chunks = list(fastcdc(file_content_bytes, min_size=min_size,
+            avg_size=avg_size, max_size=max_size, fat=True, hf=hash_function))
         end = time.perf_counter_ns()
 
         total_collisions = 0
@@ -82,14 +95,9 @@ def upload_file():
             'message': 'File was successfully uploaded.'
         })
         res.status_code = 200
-        return res
 
     except ValueError:
-        res = jsonify({
-            'message': 'min_size, avg_size or max_size isn\'t interger.'
-        })
-        res.status_code = 400
-        return res
+        return handle_not_valid_request(res, 'min_size, avg_size or max_size isn\'t interger.')
 
     except Exception as e:
         print(e)
@@ -97,3 +105,5 @@ def upload_file():
             'message': 'Something went wrong.'
         })
         res.status_code = 500
+        
+    return res
