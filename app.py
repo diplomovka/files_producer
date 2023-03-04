31e7044bbfafe42863959028b5f0d58c91f761d5
Jakub Hlavačka <jakubhlavacka123@gmail.com>
2023-03-04 11:54:07 +0100

    add correct way to specify port

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import settings
 from flask import Flask
 from blueprints.files_producer_routes import files_producer_routes
 
-app = Flask(__name__, port=settings.APP_PORT)
+app = Flask(__name__)
 app.register_blueprint(files_producer_routes, url_prefix='/file')
 
 @app.route('/')
@@ -10,4 +10,4 @@ def main_route():
     return '<p>routes /file </p>' 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=settings.APP_PORT)
