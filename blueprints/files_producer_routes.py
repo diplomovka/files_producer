8c852c21a42e5f5d246862a5e4b028eb70e888b9
Jakub Hlavačka <jakubhlavacka123@gmail.com>
2023-03-06 18:06:20 +0100

    add /load-from-fs route

--- blueprints/files_producer_routes.py
+++ blueprints/files_producer_routes.py
@@ -21,6 +21,50 @@ def create_directory(directory_name):
         os.mkdir(directory_name)
 
 
+def process_and_publish_file(producer, file_name, experiment_name, file_content_bytes,
+                            min_size, avg_size, max_size, hash_function):
+    # perform chunking and time it takes
+    start = time.perf_counter_ns()
+    chunks = list(fastcdc(file_content_bytes, min_size=min_size,
+        avg_size=avg_size, max_size=max_size, fat=True, hf=hash_function))
+    end = time.perf_counter_ns()
+
+    total_chunks = len(chunks)
+
+    for i, chunk in enumerate(chunks):
+        end_of_file = i == (total_chunks - 1)
+        file_data = FileData(file_name=file_name, chunk=chunk.data, chunk_hash=chunk.hash,
+                            chunk_serial_num=i, end_of_file=end_of_file, experiment_name=experiment_name)
+        producer.produce(topic=settings.FILES_TOPIC, key=str(uuid4()), value=file_data,
+                        on_delivery=delivery_report)
+
+    producer.flush()
+
+    experiment_dir = f'./{settings.EXPERIMENTS_DATA_DIR}/{experiment_name}'
+    create_directory(experiment_dir)
+
+    # write chunking time to file and total number of collisions to file
+    with open(f'{experiment_dir}/{experiment_name}_chunking_milisec.csv', 'a') as file:
+        file.write(f'{file_name};{(end-start) / 1000000}\n')
+
+
+def read_and_process_files(files_names, dir_path, experiment_name, 
+                           min_size, avg_size, max_size, hash_function):
+    counter = 0
+    for file_name in files_names:
+        producer.poll(0.0)
+
+        if counter % 1000 == 0:
+            print(f'{counter} files were send.')
+
+        file_relative_path = f'{dir_path}{file_name}'
+        file = open(file_relative_path, 'rb')
+        file_content_bytes = file.read()
+
+        process_and_publish_file(producer, file_name, experiment_name, file_content_bytes,
+                                min_size, avg_size, max_size, hash_function)
+
+
 time.sleep(settings.WAIT_BEFORE_START)
 
 MIN_SIZE = 64
@@ -69,30 +113,58 @@ def upload_file():
         file_content_bytes = uploaded_file.read()
 
         producer.poll(0.0)
+
+        process_and_publish_file(producer, uploaded_file.filename, experiment_name, file_content_bytes,
+                                 min_size, avg_size, max_size, hash_function)
         
-        # perform chunking and time it takes
-        start = time.perf_counter_ns()
-        chunks = list(fastcdc(file_content_bytes, min_size=min_size,
-            avg_size=avg_size, max_size=max_size, fat=True, hf=hash_function))
-        end = time.perf_counter_ns()
+        res = jsonify({
+            'message': 'File was successfully uploaded.'
+        })
+        res.status_code = 200
 
-        total_chunks = len(chunks)
+    except ValueError:
+        return handle_not_valid_request(res, 'min_size, avg_size or max_size isn\'t interger.')
 
-        for i, chunk in enumerate(chunks):
-            end_of_file = i == (total_chunks - 1)
-            file_data = FileData(file_name=uploaded_file.filename, chunk=chunk.data, chunk_hash=chunk.hash,
-                                chunk_serial_num=i, end_of_file=end_of_file, experiment_name=experiment_name)
-            producer.produce(topic=settings.FILES_TOPIC, key=str(uuid4()), value=file_data,
-                            on_delivery=delivery_report)
+    except Exception as e:
+        print(e)
+        res = jsonify({
+            'message': 'Something went wrong.'
+        })
+        res.status_code = 500
+        
+    return res
 
-        producer.flush()
 
-        experiment_dir = f'./{settings.EXPERIMENTS_DATA_DIR}/{experiment_name}'
-        create_directory(experiment_dir)
+@files_producer_routes.route('/load-from-fs', methods=['POST'])
+def load_data_from_fs():
+    res = None
 
-        # write chunking time to file and total number of collisions to file
-        with open(f'{experiment_dir}/{experiment_name}_chunking_milisec.csv', 'a') as file:
-            file.write(f'{uploaded_file.filename};{(end-start) / 1000000}\n')
+    if 'experiment_name' not in request.form:
+        return handle_not_valid_request(res, 'Provide any experiment name in experiment_name field.')
+
+    if 'hash_function' not in request.form or request.form.get('hash_function') not in HASH_FUNCTIONS:
+        return handle_not_valid_request(res, 'Provide valid hash function (sha256, md5 or sha1) in hash_function field') 
+
+    dir_path = None
+    if 'directory_path' in request.form:
+        dir_path = request.form['directory_path']
+        if not os.path.exists(dir_path) or not os.path.isdir(dir_path):
+            return handle_not_valid_request(res, 'Provide valid relative path to experiments input data')  
+    
+    try:
+        min_size = int(request.form.get('min_size')) if 'min_size' in request.form \
+            and int(request.form.get('min_size')) and int(request.form.get('min_size')) > MIN_SIZE else MIN_SIZE
+        avg_size = int(request.form.get('avg_size')) if 'avg_size' in request.form \
+            and int(request.form.get('avg_size')) and int(request.form.get('avg_size')) > AVG_SIZE else AVG_SIZE
+        max_size = int(request.form.get('max_size')) if 'max_size' in request.form \
+            and int(request.form.get('max_size')) and int(request.form.get('max_size')) > MAX_SIZE else MAX_SIZE
+        
+        experiment_name = request.form.get('experiment_name')
+        hash_function = HASH_FUNCTIONS[request.form.get('hash_function')]
+
+        files_names = os.listdir(dir_path)
+        read_and_process_files(files_names, dir_path, experiment_name, min_size,
+                               avg_size, max_size, hash_function)
 
         res = jsonify({
             'message': 'File was successfully uploaded.'
@@ -110,3 +182,4 @@ def upload_file():
         res.status_code = 500
         
     return res
+    
